39bf9ed52d9712a1f2ebb969dbc599a53916eb85
sudheer1221143 <137864049+sudheer1221143@users.noreply.github.com>
2023-08-22 14:55:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your desired region
+  region = "us-weast-1"  # Change to your desired region
 }
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name"  # Replace with your desired bucket name
-  acl    = "private"  # Adjust ACL as needed
+  acl    = "public"  # Adjust ACL as needed
 
   tags = {
     Name = "My S3 Bucket"
